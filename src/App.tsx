e76345e5549a6386808266ac361408238930ce1f
Debian live <eee1@i.ua>
2019-02-21 17:39:23 +0200

    Textareas properties tests (J2L, v.8), no ANYs ! Full TS variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,44 @@
-// https://webformyself.com/rabota-s-ref-v-react/
-
 import * as React from "react";
 
+interface IProps {}
+interface IState {
+  value_1: string;
+  value_2: string;
+  cols1: number;
+  cols2: number;
+  rows: number;
+}
 export class Main extends React.Component {
   textInput_1 = React.createRef<HTMLTextAreaElement>();
   textInput_r = React.createRef<HTMLTextAreaElement>();
-  state: any;
-  constructor(props:any) {
+  state: IState;
+  constructor(props: IProps) {
     super(props)
     this.state = {
       value_1: 'val 1',
       value_2: 'val 2',
       cols1: 44,
       cols2: 44,
-      rows: 11,
+      rows: 7,
     }
   }
 
-  handleSubmit_1 = (e: any) => {
+  handleSubmit_1 = (e: React.FormEvent) => {
     e.preventDefault();
     //console.log(e)
     //console.log(this.textInput_r.current)
     if (this.textInput_r.current) {
       console.log(this.textInput_r.current.cols)
-      console.log(this.textInput_r.current.scrollTop)
-      //this.setState({ value_1: this.textInput_1.current.value, cols1: 44, cols2: 22 })
+      //console.log(this.textInput_r.current.scrollTop)
       const sTA1value = this.textInput_1.current ? this.textInput_1.current.value : ""
+      console.log(">2: " + sTA1value );
+      
       this.setState({ value_1: sTA1value + sTA1value })
       this.textInput_r.current.cols = 48
       this.textInput_r.current.scrollTop += 40
     }
   };
-  handleOnChange = (e:any) => {
-    //e.preventDefault();
-    //alert("onChange")
-    //return true;
+  handleOnChange = () => {
     if (this.textInput_r.current) {
       this.textInput_r.current.cols = 60
       this.textInput_r.current.scrollTop += 20
@@ -42,11 +46,11 @@ export class Main extends React.Component {
   }
 
   render() {
-    const s: any = this.state;
+    const s: IState = this.state;
     const t = `line1\nline2\nl3\nl-4\nline-5\n\nRepeat: line1\nline2\nl3\nl-4\nline-5\n`
     return (
       <div>
-        <h3>Textareas properties tests (J2L, v.7)<br /></h3>
+        <h3>Textareas properties tests (J2L, v.8)<br /></h3>
         <textarea rows={s.rows} cols={s.cols1} 
           id="res"
           value={this.state.value_1} 
