95778aaca4fc1c105a875bcd6b06e03cf1581aab
Debian live <eee1@i.ua>
2019-02-21 18:43:07 +0200

    Textareas properties tests (J2L, v.10), twin scroll done

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,20 @@ interface IState {
 export class Main extends React.Component {
   textInput_from = React.createRef<HTMLTextAreaElement>();
   textInput_res = React.createRef<HTMLTextAreaElement>();
+  taText = `line1\nline2\nl3\nl-4\nline-5\n\nRepeat: line1\nline2\nl3\nl-4\nline-5\n`;
+  // taText = taText + taText + taText
+
   state: IState;
   constructor(props: IProps) {
     super(props)
+    const sFill = this.taText.repeat(4)
     this.state = {
-      text_from: 'val source',
-      text_res: 'val result',
+      text_from: sFill,
+      text_res: sFill,
       cols_from: 44,
       cols_res: 44,
       rows: 7,
-      height: 100
+      height: 220
     }
   }
 
@@ -39,8 +43,8 @@ export class Main extends React.Component {
       // console.log(this.textInput_r.current.scrollTop)
       const sText_from = this.textInput_from.current ? this.textInput_from.current.value : ""
       this.setState({text_res: sText_from + sText_from})
-      this.textInput_res.current.cols = 48
-      this.textInput_res.current.scrollTop += 40
+      // this.textInput_res.current.cols = 48
+      // this.textInput_res.current.scrollTop += 40
     } else {
       console.log('Now no "this.textInput_r.current"')
     }
@@ -51,44 +55,60 @@ export class Main extends React.Component {
       this.textInput_res.current.scrollTop += 20
     } else {
       console.log('Now no "this.textInput_r.current"')
-    }    
+    }
+  }
+  handleOnScroll_from = () => {
+    if (this.textInput_from.current) {
+      console.log(this.textInput_from.current.scrollTop)
+      if (this.textInput_res.current) {
+        this.textInput_res.current.scrollTop = this.textInput_from.current.scrollTop
+      }
+    }
+  }
+  handleOnScroll_res = () => {
+    if (this.textInput_res.current) {
+      console.log(this.textInput_res.current.scrollTop)
+      if (this.textInput_from.current) {
+        this.textInput_from.current.scrollTop = this.textInput_res.current.scrollTop
+      }
+    }
   }
 
   render() {
-    const taText = `line1\nline2\nl3\nl-4\nline-5\n\nRepeat: line1\nline2\nl3\nl-4\nline-5\n`
     const taStyle = {
       height: this.state.height+'px',
-      width: '48%',
-      border: "1px solid", 
       verticalAlign: "top",
     };
     return (
       <div>
-        <h3>Textareas properties tests (J2L, v.9)<br /></h3>
+        <h3>Textareas properties tests (J2L, v.10)<br /></h3>
         <form onSubmit={this.handleSubmit}>
         <textarea
+            className="halfsize"
             style={taStyle}
             id="ta-from"
-            ref={this.textInput_from} 
-            defaultValue={taText}
+            ref={this.textInput_from}
+            defaultValue={this.state.text_from}
+            onScroll={this.handleOnScroll_from}
           />
           {' '}
           <textarea
+            className="halfsize"
             style={taStyle}
             id="ta-to"
-            value={this.state.text_res} 
+            value={this.state.text_res}
             onClick={this.handleOnClick}
             ref={this.textInput_res}
-            readOnly={true} 
+            readOnly={true}
           />
           <br />
           <br />
-          <button>Submit</button>
+          <button>R = L x 2 (Submit)</button>
         </form>
       </div>
     );
   }
 }
 //    const s: IState = this.state;
-//          <textarea rows={s.rows} cols={s.cols1} 
-//          <textarea rows={s.rows} cols={s.cols2} 
+//          <textarea rows={s.rows} cols={s.cols1}
+//          <textarea rows={s.rows} cols={s.cols2}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as App from './App';
+import './index.css';
 
 ReactDOM.render(<App.Main />, document.getElementById('root'));
