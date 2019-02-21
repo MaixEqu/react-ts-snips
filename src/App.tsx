39521b926c76e74b84aab76e139fd0d80d79a4ad
Debian live <eee1@i.ua>
2019-02-21 18:10:18 +0200

    Textareas properties tests (J2L, v.9), TAs replaced

--- src/App.tsx
+++ src/App.tsx
@@ -2,69 +2,86 @@ import * as React from "react";
 
 interface IProps {}
 interface IState {
-  value_1: string;
-  value_2: string;
-  cols1: number;
-  cols2: number;
+  text_from: string;
+  text_res: string;
+  cols_from: number;
+  cols_res: number;
   rows: number;
+  height: number;
+  width?: number;
+  height_from?: number;
+  width_from?: number;
+  height_res?: number;
+  width_res?: number;
 }
 export class Main extends React.Component {
-  textInput_1 = React.createRef<HTMLTextAreaElement>();
-  textInput_r = React.createRef<HTMLTextAreaElement>();
+  textInput_from = React.createRef<HTMLTextAreaElement>();
+  textInput_res = React.createRef<HTMLTextAreaElement>();
   state: IState;
   constructor(props: IProps) {
     super(props)
     this.state = {
-      value_1: 'val 1',
-      value_2: 'val 2',
-      cols1: 44,
-      cols2: 44,
+      text_from: 'val source',
+      text_res: 'val result',
+      cols_from: 44,
+      cols_res: 44,
       rows: 7,
+      height: 100
     }
   }
 
-  handleSubmit_1 = (e: React.FormEvent) => {
+  handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    //console.log(e)
-    //console.log(this.textInput_r.current)
-    if (this.textInput_r.current) {
-      console.log(this.textInput_r.current.cols)
-      //console.log(this.textInput_r.current.scrollTop)
-      const sTA1value = this.textInput_1.current ? this.textInput_1.current.value : ""
-      console.log(">2: " + sTA1value );
-      
-      this.setState({ value_1: sTA1value + sTA1value })
-      this.textInput_r.current.cols = 48
-      this.textInput_r.current.scrollTop += 40
+    // console.log(e)
+    // console.log(this.textInput_r.current)
+    if (this.textInput_res.current) {
+      // console.log(this.textInput_r.current.cols)
+      // console.log(this.textInput_r.current.scrollTop)
+      const sText_from = this.textInput_from.current ? this.textInput_from.current.value : ""
+      this.setState({text_res: sText_from + sText_from})
+      this.textInput_res.current.cols = 48
+      this.textInput_res.current.scrollTop += 40
+    } else {
+      console.log('Now no "this.textInput_r.current"')
     }
   };
-  handleOnChange = () => {
-    if (this.textInput_r.current) {
-      this.textInput_r.current.cols = 60
-      this.textInput_r.current.scrollTop += 20
-    }
+  handleOnClick = () => {
+    if (this.textInput_res.current) {
+      this.textInput_res.current.cols = 60
+      this.textInput_res.current.scrollTop += 20
+    } else {
+      console.log('Now no "this.textInput_r.current"')
+    }    
   }
 
   render() {
-    const s: IState = this.state;
-    const t = `line1\nline2\nl3\nl-4\nline-5\n\nRepeat: line1\nline2\nl3\nl-4\nline-5\n`
+    const taText = `line1\nline2\nl3\nl-4\nline-5\n\nRepeat: line1\nline2\nl3\nl-4\nline-5\n`
+    const taStyle = {
+      height: this.state.height+'px',
+      width: '48%',
+      border: "1px solid", 
+      verticalAlign: "top",
+    };
     return (
       <div>
-        <h3>Textareas properties tests (J2L, v.8)<br /></h3>
-        <textarea rows={s.rows} cols={s.cols1} 
-          id="res"
-          value={this.state.value_1} 
-          onChange={this.handleOnChange}
-          onClick={this.handleOnChange}
-          ref={this.textInput_r}
-          readOnly={true} 
-        />
-        <form onSubmit={this.handleSubmit_1}>
-          <textarea rows={s.rows} cols={s.cols2} 
-            id="from"
-            ref={this.textInput_1} 
-            defaultValue={t}
+        <h3>Textareas properties tests (J2L, v.9)<br /></h3>
+        <form onSubmit={this.handleSubmit}>
+        <textarea
+            style={taStyle}
+            id="ta-from"
+            ref={this.textInput_from} 
+            defaultValue={taText}
           />
+          {' '}
+          <textarea
+            style={taStyle}
+            id="ta-to"
+            value={this.state.text_res} 
+            onClick={this.handleOnClick}
+            ref={this.textInput_res}
+            readOnly={true} 
+          />
+          <br />
           <br />
           <button>Submit</button>
         </form>
@@ -72,3 +89,6 @@ export class Main extends React.Component {
     );
   }
 }
+//    const s: IState = this.state;
+//          <textarea rows={s.rows} cols={s.cols1} 
+//          <textarea rows={s.rows} cols={s.cols2} 
